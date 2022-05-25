29ea4109e7f4a2344d3942870fb4a57293efc0a1
GuilhermeSilvaHenrique <guilherme.osilva@bandtec.com.br>
2022-05-25 05:33:05 -0300

    ajustando o timezone do sequelize

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('appblog','root','password',{
     host:'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00",
 })
 
 
