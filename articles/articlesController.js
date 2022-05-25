6323b3d8e49ed2199e6c1eceda80d303e7c6223d
GuilhermeSilvaHenrique <guilherme.osilva@bandtec.com.br>
2022-05-25 03:48:21 -0300

    adicionando outros componentes do projeto como bd, views, e a pasta public

---
+++ articles/articlesController.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.get("/articles", (req, res) => {
+    res.send("rota de artigos")
+})
+
+router.get("/admin/articles/new", (req, res) => {
+res.render("admin/articles/new");
+})
+
+
+module.exports = router;
